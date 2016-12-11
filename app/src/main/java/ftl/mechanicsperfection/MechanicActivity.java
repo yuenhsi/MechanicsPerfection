3e0beb695eadfae23af981784b297d80d0db64c8
Yuen Hsi <yuenhsi93@gmail.com>
2016-12-11 14:46:56 -0800

    initialized new project, .idea/ to .gitignore

---
+++ app/src/main/java/ftl/mechanicsperfection/MechanicActivity.java
@@ -0,0 +1,13 @@
+package ftl.mechanicsperfection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MechanicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mechanic);
+    }
+}
